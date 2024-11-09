a001e1e19e862488bf1332171cbb5eda04688923
TyronLo09 <Tyronlopez97@gmail.com>
2024-11-08 19:58:40 -0600

    Se modifico el program.cs de SocialMediaApp.api

--- SocialMediaApp.API/Program.cs
+++ SocialMediaApp.API/Program.cs
@@ -1,6 +1,23 @@
+using SocialMediaApp.Persistencia.Servicios;
+using SocialMediaApp.Persistencia.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
+//llamar al metodo que inyecta nuestras dependencias
+InyectarDependencias.ConfiguracionServicios(builder.Services);
+
+//Configurar la conexion string
+builder.Services.AddDbContext<SocialMediaDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("SocialMediaApp"))
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
