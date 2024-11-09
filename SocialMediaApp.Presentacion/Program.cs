ddc6abc661be30b16b40d93cae913c5d79cad2a3
TyronLo09 <Tyronlopez97@gmail.com>
2024-11-09 15:54:59 -0600

    cambios en el program.cs de presentacion

--- SocialMediaApp.Presentacion/Program.cs
+++ SocialMediaApp.Presentacion/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMediaApp.Persistencia.Data;
+using SocialMediaApp.Persistencia.Servicios;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//llamar al metodo que inyecta nuestras dependencias
+InyectarDependencias.ConfiguracionServicios(builder.Services);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+//Configurar la conexion string
+builder.Services.AddDbContext<SocialMediaDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("SocialMediaApp"))
+    );
+
+
 
 var app = builder.Build();
 
